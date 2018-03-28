5f1709c7656fd62a92fc9f1a17650c4278ddbec6
Lyle Mitchell <ljwmitchell@gmail.com>
2018-03-28 16:25:33 +0100

    another one down

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -8,27 +8,33 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 ### Complete these tasks:
 
-# # 1. Add `"Edinburgh Waverley"` to the end of the array
+# 1. Add `"Edinburgh Waverley"` to the end of the array
 # stops.push("Edinburgh Waverley")
-# # 2. Add `"Glasgow Queen St"` to the start of the array
-# stops.insert(0,"Glasgow Queen St")
-# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-# stops.insert(3, "Polmont")
-# # 4. Work out the index position of `"Linlithgow"`
-# stops.find_index("Linlithgow")
-# # 5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-# # 6. Delete `"Cumbernauld"` from the array by index
-#stops.delete_at(1)
-# 7. How many stops there are in the array?
 
+# 2. Add `"Glasgow Queen St"` to the start of the array
+# stops.insert(0,"Glasgow Queen St")
+
+# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.insert(3, "Polmont")
+
+# 4. Work out the index position of `"Linlithgow"`
+# stops.find_index("Linlithgow")
+
+# 5. Remove `"Livingston"` from the array using its name
+#stops.delete("Livingston")
+
+# 6. Delete `"Cumbernauld"` from the array by index
+#stops.delete_at(1)
+
+# 7. How many stops there are in the array?
+p stops.count()
 # 8. How many ways can we return `"Falkirk High"` from the array?
 
 # 9. Reverse the positions of the stops in the array
 
 # 10. Print out all the stops using a for loop
 
-p stops
+#p stops
 
 ## Exercise B
 
